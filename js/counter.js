8dd4afc918e9a6e8b9e82fcca9a25b07a470f0c7
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-10-26 14:33:03 +0200

    website

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,7 @@
-document.write("Updated: 23/10/2023&nbsp;&nbsp;|&nbsp;&nbsp;8380 links indexed.")
+document.addEventListener("DOMContentLoaded", function() {
+  const counterTextElement = document.getElementById("counterText");
+
+  if (counterTextElement) {
+    counterTextElement.innerHTML = "Updated: 26/10/2023&nbsp;&nbsp;|&nbsp;&nbsp;8380 links indexed.";
+  }
+});
