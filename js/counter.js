b19fc341a0702103929a270ff395caf4cb933597
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-12-20 19:35:29 +0100

    Update IS.bookmarks.html

--- js/counter.js
+++ js/counter.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const counterText = document.getElementById("counter-text");
 
   if (counterText) {
-    counterText.textContent = "Updated: 17/12/2023  |  8687 links indexed.";
+    counterText.textContent = "Updated: 20/12/2023  |  8708 links indexed.";
   }
 });
